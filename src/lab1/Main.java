4dcdecc9af176b059ace2bd59a3cd810471ef6c2
Mark Evans <mg17444@essex.ac.uk>
2018-01-15 20:15:35 +0000

    Enabled all methods within main

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -2,12 +2,12 @@ package lab1;
 
 public class Main {
     public static void main(String[] args) {
-//        Lab.calcInterest();
-//        Lab.calcMeans();
-//        Lab.greetName();
-//        Lab.escapeSeq();
-//        Lab.ageDiffrence();
-//        Lab.nextCharacters();
+        Lab.calcInterest();
+        Lab.calcMeans();
+        Lab.greetName();
+        Lab.escapeSeq();
+        Lab.ageDiffrence();
+        Lab.nextCharacters();
         Lab.triangleArea();
     }
 }
